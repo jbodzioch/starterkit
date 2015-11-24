5c88a1a675ef947f843fe9626f2f0df1bf3d0bef
Weronika <weronika.zietek@capgemini.com>
2015-11-24 15:23:01 +0100

    Add subtraction

--- src/main/java/MathematicalOperations.java
+++ src/main/java/MathematicalOperations.java
@@ -1,5 +1,12 @@
+import java.nio.charset.MalformedInputException;
+
 /**
  * Created by ldrygala on 2015-11-20.
  */
 public class MathematicalOperations {
+	
+	public int substraction(int minuend, int subtrahend) {
+		return minuend - subtrahend;
+	}
+
 }
--- src/test/java/MathematicalOperationsTest.java
+++ src/test/java/MathematicalOperationsTest.java
@@ -1,5 +1,51 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**
  * Created by ldrygala on 2015-11-20.
  */
+
 public class MathematicalOperationsTest {
+	
+	@Test
+	public void shouldReturnNullWhenSubstractNullAndNull() {
+		// given
+		MathematicalOperations subOperation = new MathematicalOperations();
+		// when
+		int subOperationResult = subOperation.substraction(0,0);
+		// then
+		assertEquals(0, subOperationResult);
+	}
+	
+	@Test
+	public void shouldReturnEightWhenSubstractTenandTwo() {
+		// given
+		MathematicalOperations subOperation = new MathematicalOperations();
+		// when
+		int subOperationResult = subOperation.substraction(10,2);
+		// then
+		assertEquals(8, subOperationResult);
+	}
+	
+	@Test
+	public void shouldReturnNegativeFiveWhenSubstractNullAndFive() {
+		// given
+		MathematicalOperations subOperation = new MathematicalOperations();
+		// when
+		int subOperationResult = subOperation.substraction(0,5);
+		// then
+		assertEquals(-5, subOperationResult);
+	}
+	
+	@Test
+	public void shouldReturnNegativeTwentyWhenSubstractNegativeTenAndTen() {
+		// given
+		MathematicalOperations subOperation = new MathematicalOperations();
+		// when
+		int subOperationResult = subOperation.substraction(-10,10);
+		// then
+		assertEquals(-20, subOperationResult);
+	}
+	
 }
