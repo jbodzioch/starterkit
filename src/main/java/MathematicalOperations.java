7f8439f0a528c7e5fe51ab8c75530e273d1d1ce2
Slawek Bedra <slawekbedra@gmail.com>
2015-11-24 10:41:19 +0100

    Added sum operation

--- src/main/java/MathematicalOperations.java
+++ src/main/java/MathematicalOperations.java
@@ -2,4 +2,8 @@
  * Created by ldrygala on 2015-11-20.
  */
 public class MathematicalOperations {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
 }
--- src/test/java/MathematicalOperationsTest.java
+++ src/test/java/MathematicalOperationsTest.java
@@ -1,5 +1,22 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
  * Created by ldrygala on 2015-11-20.
  */
 public class MathematicalOperationsTest {
+
+	@Test
+	public void testShouldReturnTwoWhenAddindOneAndOne() {
+		// given
+		MathematicalOperations sumOperation = new MathematicalOperations();
+		// when
+		int a = 1;
+		int b = 1;
+		int sumOperationResult = sumOperation.sum(a, b);
+		// then
+		assertEquals(2, sumOperationResult);
+
+	}
 }
